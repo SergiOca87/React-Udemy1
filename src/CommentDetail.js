4d6ce739405230c013656b9adcdf1a47f3302e0c
ocasergi@gmail.com <ocasergi@gmail.com>
2019-10-02 10:00:29 +0200

    new commit

---
+++ src/CommentDetail.js
@@ -0,0 +1,28 @@
+//React components are written in Capitals, it's convention
+import React from 'react';
+import faker from 'faker';
+
+const CommentDetail = () => {
+    return(
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img src={ faker.image.avatar() } alt="avatar" />    
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam   
+                </a>
+                <div className="metadata">
+                    <span className="date">
+                        Today at 6:00PM
+                    </span>
+                </div>
+                <div className="text">
+                    Test
+                </div>
+            </div>    
+        </div> 
+    )
+}
+
+export default CommentDetail
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Importing the comment component
+import CommentDetail from './CommentDetail';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//The css from Semantic UI is hosted inside of the public directory
+
+const App = () => {
+    return(
+        <div className="ui container comments">
+            <CommentDetail />
+
+            <CommentDetail />
+
+            <CommentDetail />
+        </div>
+    )
+}
+
+ReactDOM.render( <App />, document.querySelector('#root') );
